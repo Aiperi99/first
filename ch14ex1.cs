f5e79dd856bea33f7bd5dac80cd9dc1b614f84bf
Aiperi99 <56833179+Aiperi99@users.noreply.github.com>
2019-10-22 17:57:56 +0200

    Add files via upload

---
+++ ch14ex1.cs
@@ -0,0 +1,25 @@
+//Exercises Ch. 7 Ex.1
+using System;
+
+namespace Exercises
+{
+    
+    public class Student
+    	{
+    		string fullname;
+    		string subject;
+    		string university;
+    		string email;
+            string phoneNumber;
+    	} // end of the class Student
+    
+    //--------------------------------
+    public class Program
+    {
+    	public static void Main (string[]args)
+    	{
+    		Console.WriteLine (" It works! ");	
+    	}
+   } // end of the Program	
+}
+
